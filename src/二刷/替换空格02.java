2f682b29d26e8636106c8863838b41d146bfff56
850252329 <850252329@qq.com>
2019-08-26 12:50:22 +0800

    2shua

---
+++ src/二刷/替换空格02.java
@@ -0,0 +1,27 @@
+package 二刷;
+
+public class 替换空格02 {
+	
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("we are family");
+		System.out.println(replaceSpace(sb));
+	}
+	
+	/*
+	 * ac 100%
+	 * 运行时间：18ms
+	 * 占用内存：9564k
+	 */
+	public static String replaceSpace(StringBuffer str) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ' ') {
+				sb.append("%20");
+			}else {
+				sb.append(str.charAt(i));
+			}
+		}
+		return sb.toString();
+	}
+
+}
