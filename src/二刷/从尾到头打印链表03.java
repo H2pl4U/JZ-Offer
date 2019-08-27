ebb6fcde56959456ee376bc0eb15517a59c1c2ba
850252329 <850252329@qq.com>
2019-08-27 14:11:01 +0800

    2shua

--- src/二刷/从尾到头打印链表03.java
+++ src/二刷/从尾到头打印链表03.java
@@ -1,34 +1,34 @@
 package 二刷;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 class ListNode {
 	int val;
 	ListNode next = null;
+
 	ListNode(int val) {
 		this.val = val;
 	}
 }
 
-
 public class 从尾到头打印链表03 {
 
 	/*
-	 * ac 100%
-	 * 运行时间：17ms
-	 * 占用内存：9560k
+	 * ac 100% 运行时间：17ms 占用内存：9560k
 	 */
 	public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		Stack<Integer> stack = new Stack<Integer>();
-		while(listNode!=null) {
+		while (listNode != null) {
 			stack.add(listNode.val);
-			listNode=listNode.next;
+			listNode = listNode.next;
 		}
-		while(!stack.isEmpty()) {
+		while (!stack.isEmpty()) {
 			list.add(stack.pop());
 		}
 		return list;
 	}
+
 }
