8a8028aeb70211e56a116d2496e54f6f3cd731c9
850252329 <850252329@qq.com>
2019-08-30 23:22:01 +0800

    2shua

---
+++ src/二刷/变态跳台阶09.java
@@ -0,0 +1,24 @@
+package ¶şË¢;
+
+public class ±äÌ¬ÌøÌ¨½×09 {
+
+	// ac
+	public static int BTJumpFloor(int target) {
+		return (int) Math.pow((double) 2, (double) (target - 1));
+	}
+
+	// ac
+	public static int BTJumpFloor2(int target) {
+		if (target <= 0)
+			return 0;
+		if (target == 1)
+			return 1;
+		int a = 1;
+		int b = 2;
+		for (int i = 2; i <= target; i++) {
+			b = 2 * a;
+			a = b;
+		}
+		return b;
+	}
+}
