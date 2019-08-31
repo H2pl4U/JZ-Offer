af4e911f3e089003e24eec9842ef273a57e2e988
850252329 <850252329@qq.com>
2019-08-31 23:35:24 +0800

    2shua

---
+++ src/二刷/矩形覆盖10.java
@@ -0,0 +1,16 @@
+package ¶şË¢;
+
+public class ¾ØĞÎ¸²¸Ç10 {
+
+	//ac
+	public static int RectCover(int target) {
+		if(target==1 || target ==2) return target;
+		int f1=1,f2=1,sum=0;
+		while(target>2) {
+			sum=f1+f2;
+			f1=f2;
+			f2=sum;
+		}
+		return sum;
+	}
+}
