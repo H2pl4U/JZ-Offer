7f53a7c4f4cbd675438f468ee0ffb30ca745f4e2
850252329 <850252329@qq.com>
2020-01-29 11:51:21 +0800

    cmt

--- src/二刷/重建二叉树04.java
+++ src/二刷/重建二叉树04.java
@@ -18,5 +18,18 @@ public class ÖØ½¨¶þ²æÊ÷04 {
 		}
 		return root;
 	}
+	
+	public TreeNode reConstructBinaryTree2(int[] pre, int[] in) {
+		if(pre.length == 0 || in.length == 0) return null;
+		TreeNode root = new TreeNode(pre[0]);
+		for (int i = 0;i < in.length;i++) {
+			if (in[i] == pre[0]) {
+				root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1), Arrays.copyOfRange(in, 0, i));
+				root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i+1, pre.length), Arrays.copyOfRange(in, i+1, in.length));
+				break;
+			}
+		}
+		return root;
+	}
 
 }
